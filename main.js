8a7bf7a54482fdc4535844611c4a640a9cd25d2e
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2023-12-19 12:10:27 +0000

    Make the visual editor available as a class. Export the visual editor as a class that can add fucntionality to elements on the page. Update the example to use this class and place it into its own repository.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.scss'
+import './scss/style.scss'
 
 import { EditorState } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
@@ -7,18 +7,22 @@ import { markdownSerializer} from "./src/markdown"
 import schema from "./src/schema"
 import plugins from "./src/plugins"
 
-let state = EditorState.create({
-  doc: DOMParser.fromSchema(schema).parse(document.querySelector("#content")),
-  plugins: plugins({ schema })
-})
+export default class GovspeakVisualEditor {
+  constructor (content, editor, govspeak) {
+    let state = EditorState.create({
+      doc: DOMParser.fromSchema(schema).parse(content),
+      plugins: plugins({ schema })
+    })
 
-window.view = new EditorView(document.querySelector("#editor"), {
-  state,
-  dispatchTransaction(transaction) {
-    let newState = view.state.apply(transaction)
-    view.updateState(newState)
-    document.querySelector('pre').textContent = markdownSerializer.serialize(window.view.state.doc)
+    window.view = new EditorView(editor, {
+      state,
+      dispatchTransaction(transaction) {
+        let newState = view.state.apply(transaction)
+        view.updateState(newState)
+        govspeak.textContent = markdownSerializer.serialize(window.view.state.doc)
+      }
+    })
+
+    govspeak.textContent = markdownSerializer.serialize(window.view.state.doc)
   }
-})
-
-document.querySelector('pre').textContent = markdownSerializer.serialize(window.view.state.doc)
+}
