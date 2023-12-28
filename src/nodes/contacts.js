737968aec0c9ef690f80f78a3556b94a59957644
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-12-28 10:37:13 +0000

    Add basic support for contacts
    Contacts seem to allow pretty much any markdown within them so this should
    be fine to go as is.

---
+++ src/nodes/contacts.js
@@ -0,0 +1,32 @@
+import { wrapItem } from "prosemirror-menu"
+import { wrappingInputRule } from "prosemirror-inputrules"
+
+export const name = "contact"
+
+export const schema = {
+  content: "block+",
+  group: "block",
+  defining: true,
+  parseDOM: [{ tag: `div.contact[role="contact"][aria-label="Contact"]` }],
+  toDOM() { return ["div", { class: "contact" }, ["p", 0]] }
+}
+
+export const buildMenu = ({ menu, schema }) => {
+  menu.typeMenu.content.push(
+    wrapItem(schema.nodes[name], {
+      label: "Contact"
+    })
+  )
+}
+
+export const inputRules = (schema) => ([
+  // $C Contact
+  wrappingInputRule(/^\$C\s$/, schema.nodes[name]),
+])
+
+export const toGovspeak = (state, node) => {
+  state.write("$C\n\n")
+  state.renderInline(node)
+  state.write("$C")
+  state.closeBlock(node)
+}
