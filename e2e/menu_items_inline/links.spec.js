e65a6a1b0a8fdaa6b31af7c0ce9af0ff9867fb78
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-05-14 15:47:03 +0100

    Add email link prompt
    Add an email link button, enabled when no text is selected and a link can be inserted.
    Prompt the user for an email when the button is clicked and insert it as a link with a mailto:.
    Update the link markdown to write valid email addresses with links as is to govspeak.

--- e2e/menu_items_inline/links.spec.js
+++ e2e/menu_items_inline/links.spec.js
@@ -5,38 +5,64 @@ test.beforeEach(async ({ page }) => {
   await page.goto(VISUAL_EDITOR_URL);
 });
 
-test("link menu item disabled by default", async ({ page }) => {
-  await expect(page.getByTitle("Link", { exact: true })).toBeDisabled();
+test.describe("Link", () => {
+  test("link menu item disabled by default", async ({ page }) => {
+    await expect(page.getByTitle("Link", { exact: true })).toBeDisabled();
+  });
+
+  test("link menu item enabled with selection", async ({ page }) => {
+    await page
+      .locator("#editor")
+      .getByText("Example link", { exact: true })
+      .selectText();
+    await expect(page.getByTitle("Link", { exact: true })).toBeEnabled();
+  });
+
+  test("link menu item removes a selected link", async ({ page }) => {
+    await page
+      .locator("#editor")
+      .getByText("Example link", { exact: true })
+      .selectText();
+    await page.getByTitle("Link", { exact: true }).click();
+    await expect(
+      page.locator("#editor").getByText("Example link", { exact: true }),
+    ).not.toHaveAttribute("href");
+  });
+
+  test("link menu item adds a link to selected text", async ({ page }) => {
+    await page
+      .locator("#editor")
+      .getByText("Example link", { exact: true })
+      .selectText();
+    await page.getByTitle("Link", { exact: true }).click();
+    page.on("dialog", (dialog) => dialog.accept("example.com"));
+    await page.getByTitle("Link", { exact: true }).click();
+    await expect(
+      page.locator("#editor").getByText("Example link", { exact: true }),
+    ).toHaveAttribute("href", "example.com");
+  });
 });
 
-test("link menu item enabled with selection", async ({ page }) => {
-  await page
-    .locator("#editor")
-    .getByText("Example link", { exact: true })
-    .selectText();
-  await expect(page.getByTitle("Link", { exact: true })).toBeEnabled();
-});
+test.describe("Email link", () => {
+  test("email link menu item disabled by default", async ({ page }) => {
+    await expect(page.getByTitle("Link", { exact: true })).toBeDisabled();
+  });
 
-test("link menu item removes a selected link", async ({ page }) => {
-  await page
-    .locator("#editor")
-    .getByText("Example link", { exact: true })
-    .selectText();
-  await page.getByTitle("Link", { exact: true }).click();
-  await expect(
-    page.locator("#editor").getByText("Example link", { exact: true }),
-  ).not.toHaveAttribute("href");
-});
+  test("email link menu item enabled with selection", async ({ page }) => {
+    await page
+      .locator("#editor")
+      .getByText("Example link", { exact: true })
+      .selectText();
+    await expect(page.getByTitle("Link", { exact: true })).toBeEnabled();
+  });
 
-test("link menu item adds a link to selected text", async ({ page }) => {
-  await page
-    .locator("#editor")
-    .getByText("Example link", { exact: true })
-    .selectText();
-  await page.getByTitle("Link", { exact: true }).click();
-  page.on("dialog", (dialog) => dialog.accept("example.com"));
-  await page.getByTitle("Link", { exact: true }).click();
-  await expect(
-    page.locator("#editor").getByText("Example link", { exact: true }),
-  ).toHaveAttribute("href", "example.com");
+  test("email link menu item prompts the user for an email that it links to", async ({
+    page,
+  }) => {
+    page.on("dialog", (dialog) => dialog.accept("contact@example.com"));
+    await page.getByTitle("Email link", { exact: true }).click();
+    await expect(
+      page.locator("#editor").getByText("contact@example.com", { exact: true }),
+    ).toHaveAttribute("href", "mailto:contact@example.com");
+  });
 });
