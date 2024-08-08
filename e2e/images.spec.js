f028d7b7921ef49943afc11b5ef4220c867f7bf1
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-08-08 17:50:41 +0100

    Parse pasted image markdown
    Add a plugin to check if pasted content is image markdown text and insert the appropriate image if it is.

--- e2e/images.spec.js
+++ e2e/images.spec.js
@@ -9,3 +9,13 @@ test("loads image from the index file in the editor", async ({ page }) => {
   await expect(page.locator('#content [src="/example.jpg"]')).toBeAttached();
   await expect(page.locator('#editor [src="/example.jpg"]')).toBeVisible();
 });
+
+test("parses pasted markdown", async ({ page }) => {
+  await expect(page.locator('#editor [src="/example.jpg"]')).toHaveCount(1);
+  await page.locator('#editor [contenteditable="true"]').focus();
+  await page.keyboard.type("[Image: example.jpg]\n");
+  await page.locator("#editor").getByText("[Image: example.jpg]").selectText();
+  await page.keyboard.press("ControlOrMeta+X");
+  await page.keyboard.press("ControlOrMeta+V");
+  await expect(page.locator('#editor [src="/example.jpg"]')).toHaveCount(2);
+});
