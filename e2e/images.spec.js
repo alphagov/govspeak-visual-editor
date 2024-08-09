972841acfbd06bfe6ed8679c8a724184736515ec
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-08-09 12:01:41 +0100

    Handle pasting of invalid images
    In the case where we can't map from an image markdown to a source image, display a placeholder and a error to the user.
    This can occur when an image has been uploaded in another tab or another document entirely.

--- e2e/images.spec.js
+++ e2e/images.spec.js
@@ -28,3 +28,17 @@ test("parses pasted markdown", async ({ page }) => {
   await page.keyboard.press("ControlOrMeta+V");
   await expect(page.locator('#editor [src="/example.jpg"]')).toHaveCount(2);
 });
+
+test("invalid markdown is pasted with placeholder", async ({ page }) => {
+  await page.locator('#editor [contenteditable="true"]').focus();
+  await page.keyboard.type("[Image: example-2.jpg]\n");
+  await page
+    .locator("#editor")
+    .getByText("[Image: example-2.jpg]")
+    .selectText();
+  await page.keyboard.press("ControlOrMeta+X");
+  await page.keyboard.press("ControlOrMeta+V");
+  await expect(page.locator(".visual-editor__block--error")).toBeVisible();
+  await page.locator(".visual-editor__block--error").click();
+  await expect(page.getByText("Image not found.")).toBeVisible();
+});
