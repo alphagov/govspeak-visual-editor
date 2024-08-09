baff7656445173894b74cfcbdbd37c0e82579c31
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-08-09 12:01:41 +0100

    Introduce linting for invalid images
    In the case where we can't map from an image markdown to a source image, display a placeholder and a error to the user.
    This can occur when an image has been uploaded in another tab or another document entirely.

--- e2e/images.spec.js
+++ e2e/images.spec.js
@@ -10,6 +10,25 @@ test("loads image from the index file in the editor", async ({ page }) => {
   await expect(page.locator('#editor [src="/example.jpg"]')).toBeVisible();
 });
 
+test("invalid image from the index file in the editor is displayed with an error", async ({
+  page,
+}) => {
+  await expect(page.locator('#content [src="/example-2.jpg"]')).toBeAttached();
+  await expect(
+    page.locator(
+      '.visual-editor__highlight--image-error [src="/example-2.jpg"]',
+    ),
+  ).toBeVisible();
+  await page
+    .locator('.visual-editor__highlight--image-error [src="/example-2.jpg"]')
+    .click();
+  await expect(
+    page.getByText(
+      "Upload the image on the images tab before using it in the document.",
+    ),
+  ).toBeVisible();
+});
+
 test("parses pasted markdown", async ({ page }) => {
   await expect(page.locator('#editor [src="/example.jpg"]')).toHaveCount(1);
   await page.locator('#editor [contenteditable="true"]').focus();
@@ -18,4 +37,22 @@ test("parses pasted markdown", async ({ page }) => {
   await page.keyboard.press("ControlOrMeta+X");
   await page.keyboard.press("ControlOrMeta+V");
   await expect(page.locator('#editor [src="/example.jpg"]')).toHaveCount(2);
+});
+
+test("invalid markdown is pasted with placeholder", async ({ page }) => {
+  await page.locator('#editor [contenteditable="true"]').focus();
+  await page.keyboard.type("[Image: example-2.jpg]\n");
+  await page
+    .locator("#editor")
+    .getByText("[Image: example-2.jpg]")
+    .selectText();
+  await page.keyboard.press("ControlOrMeta+X");
+  await page.keyboard.press("ControlOrMeta+V");
+  await expect(
+    page.locator(".visual-editor__highlight--image-error [markdown]"),
+  ).toBeVisible();
+  await page
+    .locator(".visual-editor__highlight--image-error [markdown]")
+    .click();
+  await expect(page.getByText("Image not found.")).toBeVisible();
 });
